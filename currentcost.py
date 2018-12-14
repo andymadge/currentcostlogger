5637677cb1ddd01aa096e8ae6cbe9905927732af
Andy Madge <andymadge@gmail.com>
2018-12-14 22:39:07 +0000

    Refactoring

--- currentcost.py
+++ currentcost.py
@@ -7,27 +7,46 @@ from xml.etree.cElementTree import fromstring
 
 serial = serial.Serial('/dev/ttyUSB0', 57600)
 
+
+def read_serial(serial):
+    msg = serial.readline()
+    if not msg:
+        raise ValueError('Time out')
+    print(msg)
+    return msg
+
+def extract_values(xml):
+    watts = int(xml.find('ch1').find('watts').text)
+    return watts
+
+def process_xml(xml):
+        sensor = int(xml.find('sensor').text)
+
+        if sensor == 0:
+            # whole-house data
+            pass
+        elif sensor == 1:
+            # IAM 1
+            pass
+
+        watts = extract_values(xml)
+
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+        print(timestamp, watts)
+
 try:
     while True:
-        msg = serial.readline()
-        if not msg:
-            raise ValueError('Time out')
-
-        print(msg)
-
-        xml = fromstring(msg)
+        xml = fromstring(read_serial(serial))
 
         if xml.tag != 'msg':
             continue
 
         if xml.find('hist'):
+            # TODO: Write history here
             continue
 
-        watts = int(xml.find('ch1').find('watts').text)
-
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-
-        print(timestamp, watts)
+        process_xml(xml)
 
 except KeyboardInterrupt:
     sys.exit()
