e4659fe6d276bf522d78aeb38bf02ac46edf002f
Andy Madge <andymadge@gmail.com>
2018-12-15 11:53:41 +0000

    added timestamp to output

--- currentcost.py
+++ currentcost.py
@@ -8,7 +8,7 @@ from xml.etree.cElementTree import fromstring
 serial = serial.Serial('/dev/ttyUSB0', 57600)
 
 def read_serial(serial):
-    msg = serial.readline().decode('utf-8', errors='ignore')
+    msg = serial.readline().decode('utf-8', errors='ignore').rstrip()
     if not msg:
         raise ValueError('Time out')
     print(msg)
@@ -21,7 +21,11 @@ def extract_values(xml):
 
 def write_datafile(data, sensor):
     with open("data_sensor_{}.xml".format(sensor), "a") as f:
-        f.write(data)
+        # f.write(data)
+        print(now_timestamp(), data, file=f)
+
+def now_timestamp():
+    return datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
 
 def process_xml(xml, msg):
         sensor = int(xml.find('sensor').text)
@@ -30,7 +34,7 @@ def process_xml(xml, msg):
 
         watts, temperature = extract_values(xml)
 
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        timestamp = now_timestamp()
 
         # print(timestamp, watts, temperature)
 
@@ -57,14 +61,11 @@ def main():
                     # monthly history
                     write_datafile(msg, "hist_monthly")
                 continue
-
+            
             process_xml(xml, msg)
 
         except KeyboardInterrupt:
             sys.exit()
-        except xml.etree.ElementTree.ParseError:
-            continue      # can't do this cos we're already outside the while loop
-                            # need to put the try/except inside the while loop for this
 
 if __name__ == "__main__":
     main()
